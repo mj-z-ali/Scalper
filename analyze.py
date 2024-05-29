94f39c2d1745259fecb887e89a574e38b1c09ac5
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2024-05-29 04:50:38 -0500

    Enhanced function to discard resistance levels where price was surpassing resistance level between its two points

--- analyze.py
+++ analyze.py
@@ -43,37 +43,59 @@ def insert_top_bottom_columns(bar_df: pd.DataFrame) -> pd.DataFrame:
 
 def top_differences(bar_df: pd.DataFrame) -> np.array:
 
-    top_differences = np.abs(np.array([diff_matrix(matrix=current_bar_df['top'].values)
-                        for _, current_bar_df in bar_df.resample('1D') 
-                        if not current_bar_df.dropna().empty]).flatten())
+    top_diffs = np.array([])
     
-    return top_differences
+    for _, current_bar_df in bar_df.resample('1D'):
 
-def resistance_levels(bar_df: pd.DataFrame, centroids: np.array) -> np.array:
+        if not current_bar_df.dropna().empty:
 
-    top_diffs = top_differences(bar_df=bar_df)
+            n = len(current_bar_df['top'].values)
+
+            upper_row, upper_columns = np.triu_indices(n=n, k=1)
+
+            top_diffs = np.append(top_diffs, diff_matrix(matrix=current_bar_df['top'].values)[upper_row, upper_columns])
+
+    return top_diffs
+
+def resistance_levels(bar_df: pd.DataFrame, centroids: np.array) -> tuple[np.array, np.array]:
+
+    n = len(bar_df['top'].values)
+
+    top_diff_2d = diff_matrix(matrix=bar_df['top'].values)
+
+    top_diffs = np.abs(top_diff_2d.flatten())
 
     zeroes = np.zeros(top_diffs.shape[0])
 
-    top_diffs_2d = np.column_stack((top_diffs, zeroes))   
+    top_diff_points = np.column_stack((top_diffs, zeroes))
 
-    bar_tops = bar_df['top'].values
+    labels = km.k_means_model(data=top_diff_points, centroids=centroids)
 
-    n = len(bar_tops)
-
-    bar_tops_tiled = np.tile(bar_tops, (n, 1))
-
-    upper_row, upper_columns = np.triu_indices(n=n, k=1)
-
-    bar_tops_upper_tri = bar_tops_tiled[upper_row][upper_columns]
-
-    labels = km.k_means_model(data=top_diffs_2d, centroids=centroids)
+    labels_2d = labels.reshape(n,n)
 
     centroid_nearest_zero = np.argmin(centroids, axis=0)[0]
 
-    small_top_diff_indices = np.where(labels==centroid_nearest_zero)
+    break_condition = (top_diff_2d < 0) & (labels_2d != centroid_nearest_zero)
 
-    return bar_tops_upper_tri[small_top_diff_indices]
+    upper_tri_mask = np.triu(np.ones(top_diff_2d.shape, dtype=bool), k=1)
+
+    cut_off_indices = np.argmax(break_condition & upper_tri_mask, axis=1)
+
+    col_indices = np.arange(n)
+
+    cut_off_mask = (col_indices >= cut_off_indices[:, None]) & (cut_off_indices[:, None] != 0)
+
+    resistance_condition = (~cut_off_mask) & (upper_tri_mask) & (labels_2d == centroid_nearest_zero)
+
+    bar_tops = bar_df['top'].values
+
+    bar_tops_tiled = np.tile(bar_tops, (n, 1)).T
+
+    start_end_indices = np.column_stack(np.where(resistance_condition))
+
+    return bar_tops_tiled[resistance_condition], start_end_indices
+
+
 
 def diff_matrix(matrix: np.array) -> np.array:
 
@@ -83,9 +105,7 @@ def diff_matrix(matrix: np.array) -> np.array:
 
     diff_matrix = 100*(matrix_reshaped - matrix_reshaped.T) / matrix_reshaped
 
-    upper_row, upper_columns = np.triu_indices(n=n, k=1)
-
-    return diff_matrix[upper_row, upper_columns]
+    return diff_matrix
 
 def area_matrix(matrix: np.array) -> np.array:
 
@@ -131,6 +151,19 @@ def area_matrix(matrix: np.array) -> np.array:
 #                         if not five_minute_chart_df.dropna().empty]).flatten())
 # end_time_for_top_differences = time.time()
 
+medium_five_minute_df = pd.read_csv('18_12_2023_5M.csv', parse_dates=['timestamp'], index_col='timestamp')
+
+medium_five_minute_ohlctb_df = insert_top_bottom_columns(bar_df=medium_five_minute_df)
+
+top_diffs = np.abs(top_differences(bar_df=medium_five_minute_ohlctb_df))
+
+zeroes = np.zeros(top_diffs.shape[0])
+
+top_diffs_2d = np.column_stack((top_diffs, zeroes))
+
+centroids, labels, k, score = km.k_means_fit(data=top_diffs_2d, start_k=100, max_k=101)
+# print(f"centroids, labels, k, score {centroids, labels, k, score}")
+
 # areas = np.array([area_matrix(matrix=five_minute_chart_df['top'].values)
 #                         for _, five_minute_chart_df in ohlctb_df.resample('1D') 
 #                         if not five_minute_chart_df.dropna().empty]).flatten()
@@ -155,30 +188,31 @@ def area_matrix(matrix: np.array) -> np.array:
 # medium_centroids_csv = centroids.to_csv('medium_centroids.csv', index=True)
 # ohlctb_df.to_csv('out.csv', index=True)
 
-centroids = np.array(
-[[0.09664392, 0.        ],
-[1.13651666, 0.        ],
-[0.44012713, 0.        ],
-[0.23816486, 0.        ],
-[0.72944893, 0.        ],
-[0.01223435, 0.        ],
-[1.33493495, 0.        ],
-[0.92921289, 0.        ],
-[0.16389102, 0.        ],
-[0.32862124, 0.        ],
-[0.57206279, 0.        ],
-[1.19805531, 0.        ],
-[0.06704812, 0.        ],
-[1.00281727, 0.        ],
-[0.82669234, 0.        ],
-[0.38175386, 0.        ],
-[0.19932711, 0.        ],
-[0.12932954, 0.        ],
-[0.27988558, 0.        ],
-[0.64591077, 0.        ],
-[1.73549001, 0.        ],
-[0.50263636, 0.        ],
-[1.06943946, 0.        ],
-[1.26135835, 0.        ],
-[0.03902596, 0.        ]]
-)
+# centroids = np.array(
+# [[0.09664392, 0.        ],
+# [1.13651666, 0.        ],
+# [0.44012713, 0.        ],
+# [0.23816486, 0.        ],
+# [0.72944893, 0.        ],
+# [0.01223435, 0.        ],
+# [1.33493495, 0.        ],
+# [0.92921289, 0.        ],
+# [0.16389102, 0.        ],
+# [0.32862124, 0.        ],
+# [0.57206279, 0.        ],
+# [1.19805531, 0.        ],
+# [0.06704812, 0.        ],
+# [1.00281727, 0.        ],
+# [0.82669234, 0.        ],
+# [0.38175386, 0.        ],
+# [0.19932711, 0.        ],
+# [0.12932954, 0.        ],
+# [0.27988558, 0.        ],
+# [0.64591077, 0.        ],
+# [1.73549001, 0.        ],
+# [0.50263636, 0.        ],
+# [1.06943946, 0.        ],
+# [1.26135835, 0.        ],
+# [0.03902596, 0.        ]]
+# )
+
