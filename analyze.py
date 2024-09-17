915a710c4ed435cde5d34d02d6b2f7109fc8be05
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2024-09-17 05:40:26 -0500

    Progress

--- analyze.py
+++ analyze.py
@@ -3,27 +3,6 @@ import numpy as np
 from typing import Callable
 from numpy.typing import NDArray
 
-def upper_tri_mask(s: tuple[np.uint64, np.uint64]) -> NDArray[np.bool_]:
-
-    return np.triu(np.ones(s, dtype=bool), k=1)
-
-def lower_tri_mask(s: tuple[np.uint64, np.uint64]) -> NDArray[np.bool_]:
-
-    return np.tril(np.ones(s, dtype=bool), k=-1)
-
-def 
-
-def left_mask(q: NDArray[np.float64]) -> NDArray[np.bool_]:
-
-    return (q > 0) & lower_tri_mask(q.shape)
-
-def right_mask(q: NDArray[np.float64]) -> NDArray[np.bool_]:
-
-    return (q > 0) & upper_tri_mask(q.shape)
-
-
-
-
 
 def validate(i: np.uint64, l: NDArray[np.uint64], r: NDArray[np.uint64], b: NDArray[np.uint64]) -> NDArray[np.bool_]:
 
@@ -58,34 +37,48 @@ first_upper = upper_vertices[0]()
 second_upper  = upper_vertices[1](first_upper)
 
 
+def upper_tri_mask(s: tuple[np.uint64, np.uint64]) -> NDArray[np.bool_]:
 
-def barrier_points(m: NDArray[np.bool_]) -> NDArray[np.uint64]:
+    return np.triu(np.ones(s, dtype=bool), k=1)
 
-    default_point = lambda p: np.where(p == 0, len(p), p)
+def lower_tri_mask(s: tuple[np.uint64, np.uint64]) -> NDArray[np.bool_]:
 
-    return default_point(np.argmax(m, axis=1))
+    return np.tril(np.ones(s, dtype=bool), k=-1)
 
-def left_points(m: NDArray[np.bool_]) -> NDArray[np.uint64]:
+def left_mask(m: NDArray[np.bool_]) -> NDArray[np.bool_]:
 
-    default_point = lambda p: np.where(p == len(p)-1, -1, p)
+    return m & lower_tri_mask(m.shape)
 
-    return default_point(m.shape[1] - np.argmax(m[:,::-1], axis=1) - 1)
+def right_mask(m: NDArray[np.bool_]) -> NDArray[np.bool_]:
 
-def right_points(m: NDArray[np.bool_]) -> NDArray[np.uint64]:
+    return m & upper_tri_mask(m.shape)
+
+def last_points(m: NDArray[np.bool_]) -> NDArray[np.uint64]:
+
+    return m.shape[1] - np.argmax(m[:,::-1], axis=1) - 1
+
+def first_points(m: NDArray[np.bool_]) -> NDArray[np.uint64]:
 
     return np.argmax(m, axis=1)
 
-def above_resistance_line_mask(q: NDArray[np.float64]) -> Callable[[Callable], NDArray[np.bool_]]:
+def right_barrier_points(m: NDArray[np.bool_]) -> NDArray[np.uint64]:
+
+    default_points = lambda p: np.where(p == 0, len(p), p)
+
+    return default_points(first_points(right_mask(m)))
+
+def left_barrier_points(m: NDArray[np.bool_]) -> NDArray[np.uint64]:
+
+    default_points = lambda p: np.where(p == len(p)-1, -1, p)
+
+    return default_points(last_points(left_mask(m)))
+
+def barrier_points(q: NDArray[np.float64]) -> Callable[[Callable], NDArray[np.uint64]]:
 
     m = (q > 0)
 
-    return lambda d:  m & d(m.shape)
+    return lambda f: f(m)
 
-def points_above_resistance_line(q: NDArray[np.float64]):
-
-    f = above_resistance_line_mask(q)
-
-    return lambda t: t(f())
 
 def recursive_fun():
 
