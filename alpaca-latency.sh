0bf19c70e1c219866cf7570e50768820de1657bd
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2024-10-17 11:21:12 -0500

    message

--- alpaca-latency.sh
+++ alpaca-latency.sh
@@ -11,4 +11,4 @@ fi
 
 curl -o /dev/null -s -w "\nTime: %{time_total}seconds to retrieve 10,000 SPY trades from 2024-10-11. \n" --request GET --url 'https://data.alpaca.markets/v2/stocks/SPY/trades?start=2024-10-11&end=2024-10-11&limit=10000&feed=sip&sort=asc' --header "APCA-API-KEY-ID: $ALPACA_API_KEY" --header "APCA-API-SECRET-KEY: $ALPACA_SECRET_KEY" --header 'accept: application/json'
 
-curl -o /dev/null -s -w "\nTime: %{time_total}seconds to retrieve 10,000 SPY 1-Min-bars from 2024-10-11. \n" --request GET --url 'https://data.alpaca.markets/v2/stocks/SPY/bars?timeframe=1Min&start=2024-10-11&end=2024-10-11&limit=10000&adjustment=raw&feed=sip&sort=asc' --header "APCA-API-KEY-ID: $ALPACA_API_KEY" --header "APCA-API-SECRET-KEY: $ALPACA_SECRET_KEY" --header 'accept: application/json'
+curl -o /dev/null -s -w "\nTime: %{time_total}seconds to retrieve max SPY 1-Min-bars from 2024-10-11. \n" --request GET --url 'https://data.alpaca.markets/v2/stocks/SPY/bars?timeframe=1Min&start=2024-10-11&end=2024-10-11&limit=10000&adjustment=raw&feed=sip&sort=asc' --header "APCA-API-KEY-ID: $ALPACA_API_KEY" --header "APCA-API-SECRET-KEY: $ALPACA_SECRET_KEY" --header 'accept: application/json'
