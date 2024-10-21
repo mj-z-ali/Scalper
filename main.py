defbd6c31707eb8da9e79bf4b46e04299bd15e57
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2024-10-21 15:23:26 -0500

    Csv

--- main.py
+++ main.py
@@ -14,145 +14,12 @@ import asyncio
 import datetime
 
 
-
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows',None)
 np.set_printoptions(threshold=np.inf, linewidth=np.inf)
 
-def append_top_bottom_columns(bf: pd.DataFrame) -> pd.DataFrame:
-    # Bar dataframes directly from the Alpaca API do not have bar tops
-    # and bottoms information. Compute them and append it as new columns.
-    # Results in a new dataframe, leaving the original unchanged.
-
-    red = bf['close'].values <= bf['open'].values
-
-    # Create a new dataframe with the new columns.
-    return bf.assign(red=red, top=np.where(red, bf['open'], bf['close']), bottom=np.where(red, bf['close'], bf['open']))
-
-def f_create_bar_frame(bar_interval: int, bar_unit: TimeFrameUnit) -> Callable:
-
-    return lambda data_client, symbol, start_date, end_date: append_top_bottom_columns(acquire.bars(data_client, symbol, TimeFrame(bar_interval, bar_unit), start_date, end_date))
-
-def f_create_frame(data_client: REST, symbol: str): 
-
-    return lambda f_df, f_tm: f_df(data_client, symbol, f_tm('start_date'), f_tm('end_date'))
-
-def f_time(start_date: str, end_date: str) -> Callable:
-
-    data = {'start_date': start_date, 'end_date': end_date}
-
-    return lambda s: data[s]
-
-def time_based_partition(df: pd.DataFrame, interval: str) -> tuple[Callable, int]:
-    # Partition a single dataframe into a list of dataframes based on time interval.
-
-    partition_generator = map(lambda df_pair: df_pair[1], df.resample(interval))
-
-    partition = list(filter(lambda df_interval: not df_interval.dropna().empty, partition_generator))
-
-    return lambda i: partition[i], len(partition)
-
-def f_trade_frame_list(f_hd: Callable) -> Callable:
-
-    f_tf_l, _ = time_based_partition(f_hd(trade_frame), '1D')
-
-    return lambda i: f_trade_frame(f_tf_l(i))
-
-def f_bar_frame_list(f_hd: Callable, b_intv: int, b_u: TimeFrameUnit) -> tuple[Callable, int]:
-
-    f_bf_l, n_days = time_based_partition(f_hd(lambda f_tm, f_cf: bar_frame(f_tm, f_cf, b_intv, b_u)), '1D')
-
-    return lambda i: f_bar_frame(f_bf_l(i)), n_days
-
-def f_multi_day_resistance_data(f_hd: Callable, min_line_width: int, bar_interval: int, bar_unit: TimeFrameUnit) -> Callable:
-
-    f_bf_l, n_days = f_bar_frame_list(f_hd, bar_interval, bar_unit)
-
-    f_res = lambda i, f_args: resistance_data(i, min_line_width, f_bf_l(i), *f_args(i))
-
-    return lambda f_args: reduce(lambda acc, x: np.concatenate((acc, f_res(x, f_args))), range(1, n_days), f_res(0, f_args))
-
-
-def f_bar_frame(df: pd.DataFrame) -> Callable:
-
-    data = {
-        'time': df.index.values,
-        'range_x': np.arange(len(df)-1),
-        # 'top_p': df['top'].values[:-1],
-        # 'bottom_p': df['bottom'].values[:-1],
-        'high_p': df['high'].values[:-1]
-        
-    }
-
-    return lambda s: data[s]
-
-def f_trade_frame(tf: Callable) -> Callable:
-
-    data = {
-        'time': tf.index.values,
-        'price': tf['price'].values,
-        'size': tf['size'].values
-    }
-
-    return lambda s: data[s]
-
-def bar_frame(f_tm: Callable, f_cf: Callable, bar_interval: int, bar_unit: TimeFrameUnit) -> Callable:
-
-    try:
-        bf = pd.read_csv(f"{f_tm('start_date')}-to-{f_tm('end_date')}-{bar_interval}-{bar_unit.value}.csv", parse_dates=['timestamp'], index_col='timestamp')
-        
-        return bf
-    
-    except Exception as e:
-
-        bf = f_cf(f_create_bar_frame(bar_interval, bar_unit), f_tm)
-
-        bf.to_csv(f"{f_tm('start_date')}-to-{f_tm('end_date')}-{bar_interval}-{bar_unit.value}.csv", index=True)
-
-        return bf
-
-def trade_frame(f_tm: Callable, f_cf: Callable) -> Callable:
-
-    try:
-        tf = pd.read_csv(f"{f_tm('start_date')}-to-{f_tm('end_date')}-tick.csv", parse_dates=['timestamp'], index_col='timestamp')
-
-        return tf
-    
-    except Exception as e:
-
-        tf = f_cf(acquire.trades, f_tm)
-
-        tf.to_csv(f"{f_tm('start_date')}-to-{f_tm('end_date')}-tick.csv")
-
-        return tf
-    
-def historical_data(symbol: str, start_date: str, end_date: str):
-    
-    data_client = client.establish_client(client.init(paper=True))
-
-    f_tm = f_time(start_date, end_date)
-
-    f_cf = f_create_frame(data_client, symbol)
-
-    return lambda f_frame: f_frame(f_tm, f_cf)
 
 def main() -> int:
-
-    # f_hd = historical_data('SPY', '2024-05-17', '2024-08-17')
-    f_hd = historical_data('SPY', '2024-10-05', '2024-10-11')
-
-
-    f_data = f_multi_day_resistance_data(f_hd, 3, 5, TimeFrameUnit.Minute)
-
-    f_tf_l = f_trade_frame_list(f_hd)
-
-    # len?
-    f_args = lambda i: (k_rmsd(2), acceleration(f_tf_l(i)), first_upper_parabolic_area_enclosed(), second_upper_parabolic_area_enclosed(), first_lower_parabolic_area_enclosed(), second_lower_parabolic_area_enclosed(), cubic_area_enclosed(), first_euclidean_distance(), second_euclidean_distance(), first_slope(), second_slope(), first_percentage_diff(), second_percentage_diff())
-
-    res_data = f_data(f_args)
-
-    print(res_data.shape)
-    print(res_data)
 
     # create_bars_and_trades_csv(data_client=data_client, bar_interval=5, bar_unit=TimeFrameUnit.Minute, start_date="2024-05-17", end_date="2024-08-17")
     
@@ -184,6 +51,39 @@ def main() -> int:
 
     return 0
 
+def append_top_bottom_columns(bf: pd.DataFrame) -> pd.DataFrame:
+    # Bar dataframes directly from the Alpaca API do not have bar tops
+    # and bottoms information. Compute them and append it as new columns.
+    # Results in a new dataframe, leaving the original unchanged.
+
+    red = bf['close'].values <= bf['open'].values
+
+    # Create a new dataframe with the new columns.
+    return bf.assign(red=red, top=np.where(red, bf['open'], bf['close']), bottom=np.where(red, bf['close'], bf['open']))
+
+
+def f_bar_frame(df: pd.DataFrame) -> Callable:
+
+    data = {
+        'time': df.index.values,
+        'range_x': np.arange(len(df)-1),
+        'top_p': df['top'].values[:-1],
+        'bottom_p': df['bottom'].values[:-1],
+        'high_p': df['high'].values[:-1]
+        
+    }
+
+    return lambda s: data[s]
+
+def f_trade_frame(tf: Callable) -> Callable:
+
+    data = {
+        'time': tf.index.values,
+        'price': tf['price'].values,
+        'size': tf['size'].values
+    }
+
+    return lambda s: data[s]
 
 def data(symbol: str) -> Callable:
 
@@ -206,23 +106,26 @@ def bars(timeframe:TimeFrame) -> Callable:
         (len(results), lambda i: results[i])
     )[-1]
 
-async def async_fetch(t_task: tuple[int,Callable]) -> tuple[int,Callable] : 
+async def execute_tasks_concurrently(t_task: tuple[int,Callable]) -> tuple[int,Callable] : 
 
     tasks = map(t_task[1], range(t_task[0]))
         
     results = await asyncio.gather(*tasks)
 
-    return (len(results), lambda i: results[i][1])
+    return (
+        (len(results), lambda i: results[i][1]),
+        (len(results), lambda i: results[i])
+    )
 
 def concurrently(client: AsyncRest) -> tuple[Callable,Callable]:
 
-    f_trades = lambda symbol: lambda t_dates: asyncio.run(async_fetch(
+    f_trades = lambda symbol: lambda t_dates: asyncio.run(execute_tasks_concurrently(
         (t_dates[0], lambda i: asyncio.create_task(client.get_trades_async(symbol, t_dates[1](i), t_dates[1](i), limit=0xFFFFFFFF)))
-    ))
+    ))[0]
 
-    f_bars = lambda timeframe: lambda symbol: lambda t_dates: asyncio.run(async_fetch(
+    f_bars = lambda timeframe: lambda symbol: lambda t_dates: asyncio.run(execute_tasks_concurrently(
         (t_dates[0], lambda i: asyncio.create_task(client.get_bars_async(symbol, t_dates[1](i), t_dates[1](i), timeframe)))
-    ))
+    ))[0]
 
     return f_trades, f_bars
 
@@ -259,62 +162,95 @@ def f_open_market_only(t_df_list: tuple[int,Callable]) -> tuple[int,Callable]:
 
     return len(df_list), lambda i: df_list[i]
 
+
+
+def read_write_create_trades(symbol: str) -> Callable:
+
+    return ( 
+        lambda t_dates: lambda i: f'ticks/{t_dates[1](i)}-ticks-{symbol}.csv',
+        lambda t_retrieval_method: trades(t_retrieval_method)(symbol)
+    )
+
+def read_write_create_bars(timeframe: TimeFrame) -> Callable:
+
+    return lambda symbol: (
+        lambda t_dates: lambda i: f'bars/{t_dates[1](i)}-{timeframe}-{symbol}.csv',
+        lambda t_retrieval_method: bars(timeframe)(t_retrieval_method)(symbol)
+    )
+
+
 async def read_csv_concurrently(path:str):
 
-    return await asyncio.to_thread(pd.read_csv, path, parse_dates=['timestamp'], index_col='timestamp')
+    return await asyncio.to_thread(read_csv, path)
 
 async def write_csv_concurrently(df: pd.DataFrame, path:str):
 
     return await asyncio.to_thread(df.to_csv, path, index=True)
 
-async def rw_concurrently(tasks):
+def write_csv(df: pd.DataFrame, path: str) -> None:
 
-    return await asyncio.gather(*tasks)
+    return df.to_csv(path, index=True)
 
-def csv_optimized(t_data: tuple):
+def read_csv(path:str):
 
     try:
-        # read_results = await asyncio.gather(*t_data[0]())
-        # read_results = asyncio.run(asyncio.gather(*t_data[0]()))
-        read_results = asyncio.run(rw_concurrently(t_data[0]()))
-
-        return len(read_results), lambda i: read_results[i]
+        return pd.read_csv(path, parse_dates=['timestamp'], index_col='timestamp')
 
     except Exception:
+        return pd.DataFrame()
+    
+def merge_df_list(f_lists: tuple[int, int, tuple[int,Callable],tuple[int,Callable]]) -> tuple[int,Callable]:
 
-        t_df_list = t_data[1]()
-        write_tasks = t_data[2](t_df_list)
-        # _ = await asyncio.gather(*write_tasks)
-        _ = asyncio.run(rw_concurrently(write_tasks))
+    if f_lists[0] >= f_lists[2][0]: 
+        df_list = list(map(lambda i: f_lists[3][1](i), range(f_lists[1], f_lists[3][0])))
+        return len(df_list), lambda i: df_list[i]
+    elif f_lists[1] >= f_lists[3][0]: 
+        df_list = list(map(lambda i: f_lists[2][1](i), range(f_lists[0], f_lists[2][0])))
+        return len(df_list), lambda i: df_list[i]
 
-        return t_df_list
+    if f_lists[2][1](f_lists[0]).index[0] < f_lists[3][1](f_lists[1]).index[0]:
+        df_list = [f_lists[2][1](f_lists[0])] + list(map(lambda i: t_merge[1](i), range((t_merge:=merge_df_list((f_lists[0]+1, f_lists[1], f_lists[2], f_lists[3])))[0])))
+        return len(df_list), lambda i: df_list[i]
+    else:
+        df_list = [f_lists[3][1](f_lists[1])] + list(map(lambda i: t_merge[1](i), range((t_merge:=merge_df_list((f_lists[0], f_lists[1]+1, f_lists[2], f_lists[3])))[0])))
+        return len(df_list), lambda i: df_list[i]
 
-def read_write_create_optimized(client: AsyncRest):
+def read_write_create_optimized(client: AsyncRest) -> tuple[int,Callable]:
 
-    return lambda symbol: lambda t_dates: lambda f_data_type: (
-        t_rwc:= f_data_type(symbol)(t_dates)(concurrently(client)),
-        csv_optimized(
-            (
-                lambda: map(lambda i: read_csv_concurrently(t_rwc[0](i)), range(t_dates[0])),
-                lambda: t_rwc[1](symbol)(t_dates),
-                lambda t_df_list: map(lambda i: write_csv_concurrently(t_df_list[1](i), t_rwc[0](i)), range(t_df_list[0]))
-            )
-        )
+    return lambda symbol: lambda t_dates: lambda f_rwc_type: (
+        t_rwc:= f_rwc_type(symbol),
+        f_path:= t_rwc[0],
+        f_path_all_dates:= f_path(t_dates),
+        f_fetch:= t_rwc[1](concurrently(client)),
+        t_partial_empty_df_list:= asyncio.run(execute_tasks_concurrently((t_dates[0], lambda i: read_csv_concurrently(f_path_all_dates(i)))))[1],
+        failed_dates := [t_dates[1](i) for i in range(t_partial_empty_df_list[0]) if t_partial_empty_df_list[1](i).empty],
+        t_failed_dates := (len(failed_dates), lambda i : failed_dates[i]),
+        t_remaining_df_list:= f_fetch(t_failed_dates),
+        f_path_remaining_dates:= f_path((t_remaining_df_list[0], lambda i: t_remaining_df_list[1](i).index[0].date())),
+        _:= asyncio.run(execute_tasks_concurrently((t_remaining_df_list[0], lambda i: write_csv_concurrently(t_remaining_df_list[1](i),f_path_remaining_dates(i))))),
+        partial_df_list:= [df for i in range(t_partial_empty_df_list[0]) if not (df:=t_partial_empty_df_list[1](i)).empty],
+        t_partial_df_list:= (len(partial_df_list), lambda i: partial_df_list[i]),
+        merge_df_list((0,0,t_partial_df_list,t_remaining_df_list))
     )[-1]
 
-def read_write_create_trades(symbol: str) -> Callable:
+def read_write_create(client: REST) -> tuple[int,Callable]:
 
-    return lambda t_dates: lambda t_retrieval_method: ( 
-        lambda i: f'ticks/{t_dates[1](i)}-ticks-{symbol}.csv',
-        trades(t_retrieval_method)
-    )
-
-def read_write_create_bars(timeframe: TimeFrame) -> Callable:
-
-    return lambda symbol: lambda t_dates: lambda t_retrieval_method: ( 
-        lambda i: f'bars/{t_dates[1](i)}-{timeframe}-{symbol}.csv',
-        bars(timeframe)(t_retrieval_method)
-    )
+    return lambda symbol: lambda t_dates: lambda f_rwc_type: (
+        t_rwc:= f_rwc_type(symbol),
+        f_path:= t_rwc[0],
+        f_path_all_dates:= f_path(t_dates),
+        f_fetch:= t_rwc[1](synchronously(client)),
+        partial_empty_df_list:= list(map(lambda i: read_csv(f_path_all_dates(i)), range(t_dates[0]))),
+        t_partial_empty_df_list:= (len(partial_empty_df_list), lambda i: partial_empty_df_list[i]),
+        failed_dates := [t_dates[1](i) for i in range(t_partial_empty_df_list[0]) if t_partial_empty_df_list[1](i).empty],
+        t_failed_dates := (len(failed_dates), lambda i : failed_dates[i]),
+        t_remaining_df_list:= f_fetch(t_failed_dates),
+        f_path_remaining_dates:= f_path((t_remaining_df_list[0], lambda i: t_remaining_df_list[1](i).index[0].date())),
+        _:= list(map(lambda i: write_csv(t_remaining_df_list[1](i), f_path_remaining_dates(i)), range(t_remaining_df_list[0]))),
+        partial_df_list:= [df for i in range(t_partial_empty_df_list[0]) if not (df:=t_partial_empty_df_list[1](i)).empty],
+        t_partial_df_list:= (len(partial_df_list), lambda i: partial_df_list[i]),
+        merge_df_list((0,0,t_partial_df_list,t_remaining_df_list))
+    )[-1]
 
 if __name__ == "__main__":
     # main()
@@ -327,14 +263,15 @@ if __name__ == "__main__":
 
     # Fetch trades concurrently
     # tf = asyncio.run(fetch_trades_concurrently(async_rest_data_client))
-    spy_october = data('SPY')(('2024-10-11', '2024-10-11'))
+    spy_october = data('SPY')(('2024-09-01', '2024-09-30'))
     # spy_october_trades =  spy_october(trades(concurrently(async_client)))
     spy_october_csv = spy_october(read_write_create_optimized(async_client))
     # t_tf_list = spy_october_csv(read_write_create_trades)
     # print(t_tf_list[1](0).head())
-    t_bf_list = spy_october_csv(read_write_create_bars(TimeFrame(1,TimeFrameUnit.Minute).value))
+    t_bf_list = spy_october_csv(read_write_create_trades)
+    t_bf_list = spy_october_csv(read_write_create_bars(TimeFrame(5,TimeFrameUnit.Minute).value))
     # print(t_tf_list[1](0).head())
-    print(t_bf_list[1](0).head())
+    # print(t_bf_list[1](0).head())
 
     # spy_october_trades = spy_october(trades(synchronously(sync_client)))
     # spy_october_bars = spy_october(bars(TimeFrame(1,TimeFrameUnit.Minute).value)(synchronously(sync_client)))
